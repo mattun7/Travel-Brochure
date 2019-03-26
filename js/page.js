c04188e2f6ec7a5154c55759467c24fd19948da5
mattun7 <saxon773@yahoo.co.jp>
2019-03-26 22:12:36 +0900

    初期表示時のページ遷移

--- js/page.js
+++ js/page.js
@@ -1,132 +1,120 @@
-var Page = (function() {
+var Page = (function () {
 
-	var $container = $( '#container' ),
-		$bookBlock = $( '#bb-bookblock' ),
+	var $container = $('#container'),
+		$bookBlock = $('#bb-bookblock'),
 		$items = $bookBlock.children(),
-		itemsCount = $items.length,
 		current = 0,
-		bb = $( '#bb-bookblock' ).bookblock( {
-			speed : 800,
-			perspective : 2000,
-			shadowSides	: 0.8,
-			shadowFlip	: 0.4,
-			onEndFlip : function(old, page, isLimit) {
-				
+		bb = $('#bb-bookblock').bookblock({
+			speed: 800,
+			perspective: 2000,
+			shadows: true,
+			shadowSides: 0.8,
+			shadowFlip: 0.4,
+			onEndFlip: function (old, page, isLimit) {
+
 				current = page;
 				// updateNavigation
-				updateNavigation( isLimit );
+				updateNavigation(isLimit);
 				// initialize jScrollPane on the content div for the new item
-				setJSP( 'init' );
-				// destroy jScrollPane on the content div for the old item
-				setJSP( 'destroy', old );
+				setJSP('event');
 
 			}
-		} ),
-		$navNext = $( '#bb-nav-next' ).css('visibility','visible'),
-		$navPrev = $('#bb-nav-prev').css('visibility','hidden'),
+		}),
+		$navNext = $('#bb-nav-next'),
+		$navPrev = $('#bb-nav-prev'),
 		$toc = $('.TOC_p'),
-		$menu = $('#menu'),
-		$tblcontents = $( '#tblcontents' ),
-		transEndEventNames = {
-			'WebkitTransition': 'webkitTransitionEnd',
-			'MozTransition': 'transitionend',
-			'OTransition': 'oTransitionEnd',
-			'msTransition': 'MSTransitionEnd',
-			'transition': 'transitionend'
-		},
-		transEndEventName = transEndEventNames[Modernizr.prefixed('transition')],
-		supportTransitions = Modernizr.csstransitions;
+		$menu = $('#menu');
+
 	function init() {
 
-		// initialize jScrollPane on the content div of the first item
-		setJSP( 'init' );
+		setJSP('init');
 		initEvents();
-
+		let page = sessionStorage.getItem('page');
+		updateNavigation(page);
+		bb.jump(page);
 	}
-	
+
 	function initEvents() {
 
 		// add navigation events
-		$navNext.on( 'click', function() {
+		$navNext.on('click', function () {
 			bb.next();
 			return false;
-		} );
+		});
 
-		$navPrev.on( 'click', function() {
+		$navPrev.on('click', function () {
 			bb.prev();
 			return false;
-		} );
-		
+		});
+
 		// add swipe events
-		$items.on( {
-			'swipeleft'		: function( event ) {
-				if( $container.data( 'opened' ) ) {
+		$items.on({
+			'swipeleft': function (event) {
+				if ($container.data('opened')) {
 					return false;
 				}
 				bb.next();
 				return false;
 			},
-			'swiperight'	: function( event ) {
-				if( $container.data( 'opened' ) ) {
+			'swiperight': function (event) {
+				if ($container.data('opened')) {
 					return false;
 				}
 				bb.prev();
 				return false;
 			}
-		} );
+		});
 
-		$toc.on( 'click', function() {
-			var $el = $( this ),
+		$toc.on('click', function () {
+			var $el = $(this),
 				idx = $el.index(),
-				item = $el.data('item');
-			bb.jump( item );
-		} );
+				page = $el.data('item');
+			bb.jump(page);
+		});
 
-		$menu.on( 'click', function() {
-			bb.jump( 2 );
-		} );
-
-		// reinit jScrollPane on window resize
-		$( window ).on( 'debouncedresize', function() {
-			// reinitialise jScrollPane on the content div
-			setJSP( 'reinit' );
-		} );
-
+		$menu.on('click', function () {
+			bb.jump(2);
+		});
 	}
 
-	function setJSP( action, idx ) {
-		
+	function setJSP(action, idx) {
+
 		var idx = idx === undefined ? current : idx,
-			$content = $items.eq( idx ).children( 'div.content' ),
-			apiJSP = $content.data( 'jsp' );
-		
-		if( action === 'init' && apiJSP === undefined ) {
-			$content.jScrollPane({verticalGutter : 0, hideFocus : true });
-		}
-		else if( action === 'reinit' && apiJSP !== undefined ) {
-			apiJSP.reinitialise();
-		}
-		else if( action === 'destroy' && apiJSP !== undefined ) {
-			apiJSP.destroy();
-		}
+			$content = $items.eq(idx).children('div.content');
 
+		if (action === 'event') {
+			setNowPage(idx);
+		}
+		$content.jScrollPane({
+			verticalGutter: 0,
+			hideFocus: true
+		});
 	}
 
-	function updateNavigation( isLastPage ) {
-		
-		if( current === 0 ) {
-			$navNext.css('visibility','visible');
-			$navPrev.css('visibility','hidden');
-		}
-		else if( isLastPage ) {
-			$navNext.css('visibility','hidden');
-			$navPrev.css('visibility','visible');
-		}
-		else {
-			$navNext.css('visibility','visible');
-			$navPrev.css('visibility','visible');
+	// 次へボタン、戻るボタンの制御
+	function updateNavigation(isLastPage) {
+		if (current === 0) {
+			// 最初最終ページ以外の場合、次へ戻るボタンを表示
+			$navNext.css('visibility', 'visible');
+			$navPrev.css('visibility', 'hidden');
+		} else if (isLastPage) {
+			// 最終ページの場合、次へボタン非表示
+			$navNext.css('visibility', 'hidden');
+			$navPrev.css('visibility', 'visible');
+		} else {
+			// 最初のページの場合、戻るボタン非表示
+			$navNext.css('visibility', 'visible');
+			$navPrev.css('visibility', 'visible');
 		}
 	}
-	return { init : init };
+
+	// 現在のページをSessionStorageに保存
+	function setNowPage(page) {
+		sessionStorage.setItem('page', page + 1);
+	}
+
+	return {
+		init: init
+	};
 
 })();
